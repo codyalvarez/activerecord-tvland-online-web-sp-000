6b347629a2d1691592171f278ee7dd38fcbdf649
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-18 01:33:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,13 @@
-require "pry"
+# require "pry"
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
 
   def actors_list
-    self.actors.name
-    binding.pry
+    self.actors[0] + self.actors[1] + self.actors.name
+
+    #binding.pry
 
 
   end
