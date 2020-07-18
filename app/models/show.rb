65a42e8d793b54473cfdadb823f6603f13ec4874
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-18 02:01:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.actors.map do |actor|
-      self.actor.full_name
+      actor.full_name
 
 
     # grab 1st name, grab last name, grab film
